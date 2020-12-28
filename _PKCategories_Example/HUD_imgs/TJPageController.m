8911f8e45fcf9d5a1ea5052a82d80c80c6d3bd52
snail-z <40460770@qq.com>
2020-12-28 15:30:43 +0800

    update

--- _PKCategories_Example/HUD_imgs/TJPageController.m
+++ _PKCategories_Example/HUD_imgs/TJPageController.m
@@ -10,12 +10,11 @@
 
 @implementation TJPageController
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)init {
+    if (self = [super init]) {
+        
+    }
+    return self;
 }
-*/
 
 @end
